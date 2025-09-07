17f951022ddaf74cff2a32b429dd2a81423bf05e
hadel <hadel@IMP-HADEL>
2025-09-08 02:51:04 +0300

    push to master

--- centrny/Models/Content.cs
+++ centrny/Models/Content.cs
@@ -39,5 +39,9 @@ public partial class Content
 
     public string? Client { get; set; }
 
+    public string? Center { get; set; }
+
+    public string? CenterAr { get; set; }
+
     public virtual Root RootCodeNavigation { get; set; } = null!;
 }
